8be28718cb2f90977b338019e8747d32be2ca8c4
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-22 21:52:25 -0600

    primera version

---
+++ Condicionales.py
@@ -0,0 +1,14 @@
+#Condicionales
+
+numero = int(input("Digite un número"))
+
+if numero>0:
+    print("El número es positivo")
+elif numero==0:
+    print("El número es cero")
+else:
+    print("El número es negativo")
+
+print("Fin del programa")
+
+#Carolina EM
